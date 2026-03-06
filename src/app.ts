40612611689f597840c1e35c75cc18e5a3e67616
hlkim <haklae0724@gmail.com>
2026-03-06 17:08:55 +0900

    server.ts 수정

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
-import notesRoutes from "./routes/notesRoutes";
 import authRoutes from "./routes/authRoutes";
+import notesRoutes from "./routes/notesRoutes";
 
 const app = express();
 
+// CORS - 프론트 URL 허용
+app.use(cors({ origin: "*" }))
 app.use(express.json());
-
-app.use(cors());
 
 app.use("/notes", notesRoutes);
 app.use("/auth", authRoutes);
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,14 @@
-import express from "express"
-import cors from "cors"
 import dotenv from "dotenv"
 import { json } from "body-parser"
+import app from "./app"
 
 dotenv.config()
 
-const app = express()
+// const app = express()
 app.use(json())
 
 // Railway 동적 포트
 const PORT = process.env.PORT || 3000
-
-// CORS - 프론트 URL 허용
-app.use(cors({ origin: "*" }))
-
-// 예시 라우트
-app.get("/notes", (req, res) => {
-  res.json([{ id: 1, title: "Hello", content: "World" }])
-})
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
